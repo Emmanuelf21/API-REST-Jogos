c59e2d9a7c11b3134f3843d860228b2292061a03
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2024-06-26 15:08:55 -0300

    first commit

---
+++ desafio/games-list/src/main/java/me/games/games_list/GamesListApplication.java
@@ -0,0 +1,13 @@
+package me.games.games_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesListApplication.class, args);
+	}
+
+}
